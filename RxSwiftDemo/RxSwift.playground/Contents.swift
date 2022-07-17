a8d5152d6386508b6546f3e16e747ccdd60eb82f
Marco Alonso <marco@garagecoders.net>
2022-07-17 14:41:25 -0500

    Publish Subjects creado

--- RxSwiftDemo/RxSwift.playground/Contents.swift
+++ RxSwiftDemo/RxSwift.playground/Contents.swift
@@ -48,8 +48,8 @@ let subscription5 = observable5.subscribe({ (event: Event<String>) in
 
 //Necesitamos desechar la subscripcion una vez que se termina de usar para evitar fugas o pérdidas de memoria, esta es la forma mas facil de desechar las subscripciones pero hay otra forma, ya que tenemos mas de 1 subscripcion
 subscription5.dispose()
- */
-
+ 
+// MARK: - Bolsa de desechos
 //Se crea una bolsa de desechos
 let disposeBag = DisposeBag()
 // MARK: - 1.- Crearemos una secuencia observable, suscribirse a esa secuencia con un controlador de eventos y deshacerse de la suscripción cuando haya terminado.
@@ -69,3 +69,28 @@ Observable<String>.create { observer in
     return Disposables.create() //esta es la forma de crear la bolsa de desechos
 }.subscribe(onNext: { print($0) }, onError: { print($0) }, onCompleted: {print("Completed :) ")}, onDisposed: {print("Disposed :/ ")})
     .disposed(by: disposeBag)
+ */
+
+
+// MARK: - Subjects
+///Un sujeto es un tipo reactivo que es tanto una secuencia observable como un observador. Pueden aceptar suscripciones y emitir eventos, así como agregar nuevos elementos a la secuencia. Hay cuatro tipos de asunto: PublishSubject , BehaviorSubject , Relay(Variable), ReplaySubject
+let disposeBag = DisposeBag()
+
+let subject = PublishSubject<String>()
+
+//Si mandamos un evento antes de crear la subscripcion, no se verá reflejada
+subject.onNext("Issue 1") //Nunca se mostrará en  consola
+
+subject.subscribe { event in
+    print(event)
+}
+
+subject.onNext("Issue 2")
+subject.onNext("Issue 3")
+subject.onCompleted() //se termina la secuencia de eventos
+
+subject.dispose() //al llamar a desechar, ya no habrá mas métodos onNext, ni tampoco onCompleted
+
+subject.onCompleted()//ya no se mostrará ya que ya se desecho la subscripcion
+
+subject.onNext("Issue 4") //ya no se mostrará ya que ya se desecho la subscripcion
